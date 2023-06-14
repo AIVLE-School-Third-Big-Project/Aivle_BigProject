b0f7e13196e8929300875a9c50f30af3d4bfe02a
KymCat <kohina1452@gmail.com>
2023-06-14 16:22:34 +0900

    feat : 회원가입 페이지 추가 및 로직 추가

--- Big/apps/login/views.py
+++ Big/apps/login/views.py
@@ -16,6 +16,18 @@ def index(request) :
 def register(request) :
     return render(request, 'login/register.html')
     
+# id 중복 검사
+class idInspectionView(View) :
+    def post(self, request) :
+            id = json.loads(request.body)['id']
+            
+            # 이미 사번이 있는 경우
+            if models.User.objects.filter(id=id).exists():
+                return JsonResponse({"message" : "ALREADY_EXISTS"}, status = 400)
+            else :
+                return JsonResponse({"message" : "SUCCESS"}, status = 201)
+            
+        
 # 로그인 검사
 class loginView(View) :
     def post(self, request) :
@@ -52,17 +64,18 @@ class registerView(View) :
             
             id = data['id']
             pw = data['pw']
+            pwVerify = data['pw-verify']
             region = data['region']
             category = data['category']
-            
-            # 이미 사번이 있는 경우
-            if models.User.objects.filter(id=id).exists():
-                return JsonResponse({"message" : "ALREADY_EXISTS"}, status = 400)
 
             # 비밀번호 8 ~ 25글자
             regex_pw = '\S{8,25}'
             if not re.match(regex_pw, pw) :
                 return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
+            
+            # 비밀번호, 비밀번호 일치 X
+            if pw != pwVerify :
+                return JsonResponse({"message" : "INVALID_PASSWORD_VERIFY"}, status = 400)
             
             # 비밀번호 해싱
             pw = data['pw'].encode('utf-8')
@@ -70,11 +83,11 @@ class registerView(View) :
             
             # db에 추가
             models.User.objects.create(id = id, pw = pw_crypt, region = region, category = category)
-            return JsonResponse({"message" : "SUCCESS"}, status = 201)
+            return JsonResponse({"redirect_url" : "/"}, status = 201)
             
         # 입력 오류 => 하나 이상 비어있을 경우
         except KeyError :
-            return JsonResponse({"message" : "KEY_EROOR"}, status = 400)    
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)    
         # json 디코드 오류
         except json.JSONDecodeError as e:
             return JsonResponse({"message" : "JSON_DECODE_ERROR"}, status = 400)
--- Big/apps/sessionManagement/views.py
+++ Big/apps/sessionManagement/views.py
@@ -7,9 +7,9 @@ class CheckSessionExpiryMiddleware:
 
     def __call__(self, request):
         
-        # 세션 만료 + 로그인/로그인검사 URL이 아닐 때
-        if 'user' not in request.session and request.path != '/' \
-            and request.path != '/login/submit/' :
+        # 세션 만료 + 로그인/로그인검사/회원가입/회원가입검사 URL이 아닐 때
+        if 'user' not in request.session and \
+            request.path not in ['/', '/login/submit/', '/register/', '/register/submit/', '/register/id_inspection/'] :
             
             return render(request, 'session/sessionAlert.html')  # 세션 만료 시 로그인 화면으로 이동
 
